08abf44ad57d24be65f0bad1e03b2b71d8218a52
Brandon Lavigne <brandonlavignebl@gmail.com>
2019-04-14 16:57:42 -0700

    health fixes

--- UCISpringGameJam2019/Assets/Scripts/Health/PlayerHealth.cs
+++ UCISpringGameJam2019/Assets/Scripts/Health/PlayerHealth.cs
@@ -46,6 +46,6 @@ public class PlayerHealth : Health
         EnemySpawner spawner = FindObjectOfType<EnemySpawner>();
         spawner.canSpawn = false;
 
-        SceneManager.LoadScene("GameOverScreen");
+        SceneManager.LoadScene(0);
     }
 }
