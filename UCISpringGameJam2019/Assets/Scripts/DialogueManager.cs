d17d17c076bb16a51c200a7359b09de6acb9fbeb
Bryan <bryankhamly@gmail.com>
2019-04-14 13:07:30 -0700

    Very Simple Dialogue

---
+++ UCISpringGameJam2019/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Player player;
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+
+    public void ShowDialoguePanel(bool x)
+    {
+        dialoguePanel.SetActive(x);
+    }
+
+    public void SetDialogueText(string t)
+    {
+        dialogueText.text = t;
+    }
+}
---
+++ UCISpringGameJam2019/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public string dialogue;
+    DialogueManager manager;
+
+    void Start()
+    {
+        manager = FindObjectOfType<DialogueManager>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CheckLayer("Player"))
+        {
+            manager.ShowDialoguePanel(true);
+            manager.SetDialogueText(dialogue);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CheckLayer("Player"))
+        {
+            manager.ShowDialoguePanel(false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
